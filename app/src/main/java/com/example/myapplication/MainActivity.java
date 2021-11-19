bccb77844a44e31447f688b66103ddb9847dd756
Porom <porom.kamal@gmail.com>
2021-11-19 18:54:39 -0500

    - Implemented Database connection.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Buyer test = new Buyer(-100, "Test Object", "password", "Test",
+                "Object", "testobject@test.com");
     }
 }
