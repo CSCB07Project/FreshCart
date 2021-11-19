4a78ecbc3377a075b7e46c56773f59b4ae9743ec
Andrew Effendi <andrewamadeo20@gmail.com>
2021-11-19 13:44:49 -0500

    implement firebase for Account class

--- app/src/main/java/com/example/myapplication/Account.java
+++ app/src/main/java/com/example/myapplication/Account.java
@@ -1,35 +1,51 @@
 package com.example.myapplication;
 
 public abstract class Account {
+    int userid;
     String username;
     String password;
     String lastName;
     String firstName;
     String emailAddress;
 
-    Account(String username, String password, String firstName, String lastName, String emailAddress) {
+    Account(int userid, String username, String password, String firstName, String lastName, String emailAddress) {
+        this.userid = userid;
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.emailAddress = emailAddress;
+        updateToDatabase(); // temporary (write to database need to be implemented in subclasses)
+    }
+
+    private void updateToDatabase(){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("Users").child(userid).setValue(this);
     }
 
     protected void changeUsername(String newUsername) {
         username = newUsername;
+        updateToDatabase();
     }
 
     protected void changePassword(String oldPassword, String newPassword) {
         if (oldPassword.equals(password)) password = newPassword;
+        updateToDatabase();
     }
 
     protected void changeName(String newFirstName, String newLastName) {
         firstName = newFirstName;
         lastName = newLastName;
+        updateToDatabase();
     }
 
-    protected void changeEmail(String newEmail) {
-        username = newEmail;
+    protected void changeEmail(String newEmailAddress) {
+        emailAddress = newEmailAddress;
+        updateToDatabase();
+    }
+
+    public int getUserid() {
+        return userid;
     }
 
     public String getUsername() {
