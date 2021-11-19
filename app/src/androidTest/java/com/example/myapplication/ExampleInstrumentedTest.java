062e1e56b76e0eead407bc4c9b57870386b67341
Porom <porom.kamal@gmail.com>
2021-11-19 18:54:39 -0500

    - Implemented Database connection.

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
-import android.content.Context;
+import static org.junit.Assert.assertTrue;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -18,9 +18,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+    public void testBuyerAccount() {
+        //Test through buyerClass
+        //(Negative ID to prevent conflicts with existing data)
+
+        assertTrue(true);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Buyer.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+
+public class Buyer extends Account{
+    public Buyer(int userid, String username, String password, String firstName, String lastName, String emailAddress){
+        super(userid, username, password, firstName, lastName, emailAddress);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
