6601b7ce72ed4be08bd303ab9e9c51564ccc93ad
Porom <porom.kamal@gmail.com>
2021-11-22 02:13:48 -0500

    Added intro screen with UI from figma. Implemented 4 Second delay between intro screen and login screen. Will implement another activity which will be in between intro screen and login screen which checks for user already logged in.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,7 +12,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Buyer test = new Buyer(-100, "Test Object", "password", "Test",
-                "Object", "testobject@test.com");
+        setContentView(R.layout.introscreen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switchToLoginPage();
+            }
+        }, 4000);
+    }
+
+    public void switchToLoginPage(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
