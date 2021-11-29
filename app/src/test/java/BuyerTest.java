2b28724de89cdc3c70a57c4b066e801e963cc2e8
Porom <porom.kamal@gmail.com>
2021-11-28 23:00:54 -0500

    Removed all firebase calls from classes to keep the models pure, and allow mockito testing without firebase init errors.

---
+++ app/src/test/java/BuyerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.example.myapplication.Buyer;
+import com.google.firebase.database.FirebaseDatabase;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BuyerTest {
+    @Test
+    public void testChangeUsername(){
+        Buyer test = new Buyer("1", "test","test","test","test","test");
+        assertTrue(true);
+    }
+
+}
