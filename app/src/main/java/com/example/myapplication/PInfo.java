a3d8130c445c40a342e61ca7b65e70f59d98d8b6
Nandhakishore K.S <n.krishnamurthy@mail.utoronto.ca>
2021-12-01 12:25:28 -0500

    Added Images

--- app/src/main/java/com/example/myapplication/PInfo.java
+++ app/src/main/java/com/example/myapplication/PInfo.java
@@ -122,64 +122,64 @@ public class PInfo extends AppCompatActivity {
     }
 
     private void initImageBitmaps(){
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2016/10/07/14/11/tangerines-1721633_1280.jpg");
         mImages.add("Orange");
-        mDesc.add("A bag of 12 oranges");
+        mDesc.add("A bag of 5 oranges");
         mPrice.add("5");
 
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2013/07/05/12/12/apples-143457_1280.jpg");
         mImages.add("Apple");
-        mDesc.add("A bag of 12 apples");
+        mDesc.add("A bag of 6 apples");
         mPrice.add("6");
 
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2015/08/18/23/09/bananas-895072_1280.jpg");
         mImages.add("Banana");
         mDesc.add("A bag of 12 bananas");
         mPrice.add("7");
 
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830_1280.jpg");
         mImages.add("Lemon");
-        mDesc.add("A bag of 12 lemons");
+        mDesc.add("A bag of 10 lemons");
         mPrice.add("8");
 
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mImages.add("Orange");
-        mDesc.add("A bag of 12 oranges");
-        mPrice.add("5");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2012/02/28/15/43/rice-18431_1280.jpg");
+        mImages.add("Basmati Rice");
+        mDesc.add("A bag of Basmati Rice");
+        mPrice.add("20");
 
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mImages.add("Apple");
-        mDesc.add("A bag of 12 apples");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2016/07/16/16/30/spinach-1522283_1280.jpg");
+        mImages.add("Spinach");
+        mDesc.add("A bag of Spinach");
         mPrice.add("6");
 
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mImages.add("Banana");
-        mDesc.add("A bag of 12 bananas");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2018/05/21/21/23/garlic-3419544_1280.jpg");
+        mImages.add("Garlic");
+        mDesc.add("A packet of Garlic");
         mPrice.add("7");
 
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mImages.add("Lemon");
-        mDesc.add("A bag of 12 lemons");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2016/07/11/00/18/carrots-1508847_1280.jpg");
+        mImages.add("Carrot");
+        mDesc.add("A bag of 12 Carrots");
         mPrice.add("8");
 
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mImages.add("Orange");
-        mDesc.add("A bag of 12 oranges");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2014/08/06/20/32/potatoes-411975_1280.jpg");
+        mImages.add("Potato");
+        mDesc.add("A bag of 10 Potatoes");
         mPrice.add("5");
 
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mImages.add("Apple");
-        mDesc.add("A bag of 12 apples");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2018/12/07/00/19/savoy-3860933_1280.jpg");
+        mImages.add("Cabbage");
+        mDesc.add("A bag of Cabbage");
         mPrice.add("6");
 
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mImages.add("Banana");
-        mDesc.add("A bag of 12 bananas");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2016/05/12/16/34/ginger-1388002_1280.jpg");
+        mImages.add("Ginger");
+        mDesc.add("A packet of Ginger");
         mPrice.add("7");
 
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mImages.add("Lemon");
-        mDesc.add("A bag of 12 lemons");
+        mImageUrls.add("https://cdn.pixabay.com/photo/2016/05/11/20/35/bell-peppers-1386467_1280.jpg");
+        mImages.add("Red Bell Peppers");
+        mDesc.add("A bag of 6 Red Bell Peppers");
         mPrice.add("8");
 
         initRecyclerView();
