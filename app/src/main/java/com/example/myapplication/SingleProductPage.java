5e38483de52f6fb990c1eb82889f1ccb3b067f32
Jas0nks <jasonkenneth02@gmail.com>
2021-12-02 12:56:19 -0500

    removed all unneccesary comments

--- app/src/main/java/com/example/myapplication/SingleProductPage.java
+++ app/src/main/java/com/example/myapplication/SingleProductPage.java
@@ -15,7 +15,6 @@ public class SingleProductPage extends AppCompatActivity {
     static int count = 0;
     FloatingActionButton add;
     FloatingActionButton minus;
-    //TextView incrementdecrement = (TextView) findViewById(R.id.itemcounter);
     TextView incrementdecrement;
 
     @Override
@@ -23,97 +22,38 @@ public class SingleProductPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_product_page);
         String productimage = "";
-        String productname= "";
-        String productprice= "";
+        String productname = "";
+        String productprice = "";
         //String productdescription = "";
         //String productid = "";
-       // String productstore = "";
+
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             productimage = extras.getString("mImages");
             productname = extras.getString("mImageNames");
             productprice = "$" + extras.getString("mPrice");
-           // productdescription = extras.getString("productDescription");
+            // productdescription = extras.getString("productDescription");
             //productid = extras.getString("productID");
         }
 
-            TextView tv = (TextView) findViewById(R.id.ProductNameText);
-            tv.setText(productname);
-            TextView tv1 = (TextView) findViewById(R.id.productnameinfo);
-            tv1.setText(productname);
-            TextView tv2 = (TextView) findViewById(R.id.productdescriptioninfo);
-            //tv2.setText(productdescription);
-            TextView tv3 = (TextView) findViewById(R.id.productpriceinfo);
-            tv3.setText(productprice);
-            incrementdecrement = (TextView)findViewById(R.id.itemcounter);
-            incrementdecrement.setText(String.valueOf(count));
-            ImageView iv = (ImageView) findViewById(R.id.productpictureiv);
-            Glide.with(this)
-                    .load(productimage)
-                    .placeholder(R.drawable.cartlogo)
-                    .dontAnimate()
-                    .into(iv);
+        TextView tv = (TextView) findViewById(R.id.ProductNameText);
+        tv.setText(productname);
+        TextView tv1 = (TextView) findViewById(R.id.productnameinfo);
+        tv1.setText(productname);
+        TextView tv2 = (TextView) findViewById(R.id.productdescriptioninfo);
+        //tv2.setText(productdescription);
+        TextView tv3 = (TextView) findViewById(R.id.productpriceinfo);
+        tv3.setText(productprice);
+        incrementdecrement = (TextView) findViewById(R.id.itemcounter);
+        incrementdecrement.setText(String.valueOf(count));
+        ImageView iv = (ImageView) findViewById(R.id.productpictureiv);
+        Glide.with(this)
+                .load(productimage)
+                .placeholder(R.drawable.cartlogo)
+                .dontAnimate()
+                .into(iv);
 
-//            String finalProductid = productid;
-//            String finalProductname = productname;
-//            String finalProductdescription = productdescription;
-//            String finalProductprice = productprice;
-//            String finalProductimage = productimage;
-//            String finalProductstore = productstore;
-//
-//            FirebaseDatabase.getInstance().getReference("Product").addListenerForSingleValueEvent(new ValueEventListener() {
-//                @Override
-//                public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                    for (DataSnapshot item : snapshot.getChildren()) {
-//                        String productimage = "";
-//                        String productdescription = "";
-//                        String productname = "";
-//                        String productprice = "";
-//                        String productid = "";
-//                        String productstore = "";
-//                        if (item.hasChild("productDescription"))
-//                            productdescription = item.child("productDescription").getValue().toString();
-//                            tv2.setText(productdescription);
-//                        if (item.hasChild("productName"))
-//                            productname = item.child("productName").getValue().toString();
-//                            tv.setText(productname);
-//                            tv1.setText(productname);
-//                        if (item.hasChild("productPrice"))
-//                            productprice = item.child("productPrice").getValue().toString();
-//                            tv3.setText(productprice);
-//                        if (item.hasChild("productImage"))
-//                            productimage = item.child("productImage").getValue().toString();
-//                        if(item.hasChild("productID"))
-//                            productid = item.child("productID").getValue().toString();
-//                        if(item.hasChild("store"))
-//                            productstore = item.child("store").getValue().toString();
-//                    }
-//
-//
-//                }
-//
-//                @Override
-//                public void onCancelled(@NonNull DatabaseError error) {
-//
-//                }
-//            });
-
-
-
-        //  FirebaseDatabase.getInstance().getReference("Product").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-        //for (DataSnapshot item : snapshot.getChildren()) {
-//                    if (item.hasChild("productDescription"))
-//                        tv2.setText(item.child("productDescription").getValue().toString());
-//                    if (item.hasChild("productName"))
-//                        tv1.setText(item.child("productName").getValue().toString());
-//                    if(item.hasChild("productPrice"))
-//                        tv3.setText(item.child("productPrice").getValue().toString());
-
-        //  }
 
         add = (FloatingActionButton) findViewById((R.id.additembutton));
         add.setOnClickListener(new View.OnClickListener() {
@@ -122,18 +62,14 @@ public class SingleProductPage extends AppCompatActivity {
                 count = count + 1;
                 incrementdecrement.setText(String.valueOf(count));
             }
-//                int count = 0;
-//                count++;
-//                TextView counter = (TextView) findViewById(R.id.itemcounter);
-//                counter.setText(String.valueOf(count));
 
 
         });
         minus = (FloatingActionButton) findViewById((R.id.minusitembutton));
-        minus.setOnClickListener(new View.OnClickListener(){
+        minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(count != 0) {
+                if (count != 0) {
                     count = count - 1;
                     incrementdecrement.setText(String.valueOf(count));
                 }
