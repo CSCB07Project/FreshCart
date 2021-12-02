7909b2e9ec37405999a2a0aceb9022feb00c9d9a
Jas0nks <jasonkenneth02@gmail.com>
2021-12-02 12:51:07 -0500

    fixed some aspects of the order view page xml file and slight changes to the implementation

--- app/src/main/java/com/example/myapplication/SingleProductPage.java
+++ app/src/main/java/com/example/myapplication/SingleProductPage.java
@@ -1,36 +1,20 @@
 package com.example.myapplication;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class SingleProductPage extends AppCompatActivity {
     static int count = 0;
-    Button add;
-    Button minus;
+    FloatingActionButton add;
+    FloatingActionButton minus;
     //TextView incrementdecrement = (TextView) findViewById(R.id.itemcounter);
     TextView incrementdecrement;
 
@@ -49,7 +33,7 @@ public class SingleProductPage extends AppCompatActivity {
         if (extras != null) {
             productimage = extras.getString("mImages");
             productname = extras.getString("mImageNames");
-            productprice = extras.getString("mPrice");
+            productprice = "$" + extras.getString("mPrice");
            // productdescription = extras.getString("productDescription");
             //productid = extras.getString("productID");
         }
@@ -131,7 +115,7 @@ public class SingleProductPage extends AppCompatActivity {
 
         //  }
 
-        add = (Button) findViewById((R.id.additembutton));
+        add = (FloatingActionButton) findViewById((R.id.additembutton));
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -145,7 +129,7 @@ public class SingleProductPage extends AppCompatActivity {
 
 
         });
-        minus = (Button) findViewById((R.id.minusitembutton));
+        minus = (FloatingActionButton) findViewById((R.id.minusitembutton));
         minus.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
