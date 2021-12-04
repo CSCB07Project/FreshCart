364ce3b396a114d2ba1f541f449886179cf93e4d
Nandhakishore K.S <n.krishnamurthy@mail.utoronto.ca>
2021-12-04 03:01:09 -0500

    Added Firebase

--- app/src/main/java/com/example/myapplication/PInfo.java
+++ app/src/main/java/com/example/myapplication/PInfo.java
@@ -83,7 +83,7 @@ public class PInfo extends AppCompatActivity {
             }
         });
 
-        FirebaseDatabase.getInstance().getReference("Seller").child(storeId).addListenerForSingleValueEvent(new ValueEventListener() {
+        FirebaseDatabase.getInstance().getReference("Store").child(storeId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.hasChild("storeName"))
@@ -106,102 +106,49 @@ public class PInfo extends AppCompatActivity {
                     storeCity = snapshot.child("storeCity").getValue().toString();
                 if (snapshot.hasChild("storePostal"))
                     storePostal = snapshot.child("storePostal").getValue().toString();
+                if (snapshot.hasChild("Products")){
+                    for (DataSnapshot ds : snapshot.child("Products").getChildren())
+                        products.add(ds.getValue().toString());
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+
+
+        });
+
+        FirebaseDatabase.getInstance().getReference("Products").child(storeId).addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(String p : products){
+                    String name = snapshot.child(p).child("productName").getValue().toString();
+                    String description = snapshot.child(p).child("productDescription").getValue().toString();
+                    String price = snapshot.child(p).child("productPrice").getValue().toString();
+                    String image = snapshot.child(p).child("productImage").getValue().toString();
+                    mImages.add(name);
+                    mDesc.add(description);
+                    mPrice.add(price);
+                    mImageUrls.add(image);
+                    Log.d("productName", name);
+                }
+                Log.d("produ", String.valueOf(mImages.size()));
+                RecyclerView recyclerView = findViewById(R.id.recyclerViewBuyer1);
+                RecyclerViewAdapter2 adapter = new RecyclerViewAdapter2(mImages, mImageUrls, mPrice, mDesc,mId, storeName1, PInfo.this);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new GridLayoutManager(PInfo.this, 2));
+                Log.d("produ", String.valueOf(mImages.size()));
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-
-
         });
 
-        initImageBitmaps();
 
 
-    }
-
-    private void initImageBitmaps(){
-        mImageUrls.add("https://cdn.pixabay.com/photo/2016/10/07/14/11/tangerines-1721633_1280.jpg");
-        mImages.add("Orange");
-        mDesc.add("A bag of 5 oranges");
-        mPrice.add("5");
-        mId.add("0");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2013/07/05/12/12/apples-143457_1280.jpg");
-        mImages.add("Apple");
-        mDesc.add("A bag of 6 apples");
-        mPrice.add("6");
-        mId.add("1");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2015/08/18/23/09/bananas-895072_1280.jpg");
-        mImages.add("Banana");
-        mDesc.add("A bag of 12 bananas");
-        mPrice.add("7");
-        mId.add("2");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2017/02/05/12/31/lemons-2039830_1280.jpg");
-        mImages.add("Lemon");
-        mDesc.add("A bag of 10 lemons");
-        mPrice.add("8");
-        mId.add("3");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2012/02/28/15/43/rice-18431_1280.jpg");
-        mImages.add("Rice");
-        mDesc.add("A bag of Basmati Rice");
-        mPrice.add("20");
-        mId.add("4");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2016/07/16/16/30/spinach-1522283_1280.jpg");
-        mImages.add("Spinach");
-        mDesc.add("A bag of Spinach");
-        mPrice.add("6");
-        mId.add("5");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2018/05/21/21/23/garlic-3419544_1280.jpg");
-        mImages.add("Garlic");
-        mDesc.add("A packet of Garlic");
-        mPrice.add("7");
-        mId.add("6");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2016/07/11/00/18/carrots-1508847_1280.jpg");
-        mImages.add("Carrot");
-        mDesc.add("A bag of 12 Carrots");
-        mPrice.add("8");
-        mId.add("7");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2014/08/06/20/32/potatoes-411975_1280.jpg");
-        mImages.add("Potato");
-        mDesc.add("A bag of 10 Potatoes");
-        mPrice.add("5");
-        mId.add("8");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2018/12/07/00/19/savoy-3860933_1280.jpg");
-        mImages.add("Cabbage");
-        mDesc.add("A bag of Cabbage");
-        mPrice.add("6");
-        mId.add("9");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2016/05/12/16/34/ginger-1388002_1280.jpg");
-        mImages.add("Ginger");
-        mDesc.add("A packet of Ginger");
-        mPrice.add("7");
-        mId.add("10");
-
-        mImageUrls.add("https://cdn.pixabay.com/photo/2016/05/11/20/35/bell-peppers-1386467_1280.jpg");
-        mImages.add("Bell Peppers");
-        mDesc.add("A bag of 6 Red Bell Peppers");
-        mPrice.add("8");
-        mId.add("11");
-
-        initRecyclerView();
-    }
-
-    private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.recyclerViewBuyer1);
-        RecyclerViewAdapter2 adapter = new RecyclerViewAdapter2(mImages, mImageUrls, mPrice, mDesc,mId, storeName1, this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 }
