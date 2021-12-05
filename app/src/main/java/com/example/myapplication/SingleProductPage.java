0a76e3c6df839ccd26ae38f29185c023bda7abb7
Jas0nks <jasonkenneth02@gmail.com>
2021-12-05 15:13:16 -0500

    I think everything should be fine except line 119 which keeps giving a null pointer exception

--- app/src/main/java/com/example/myapplication/SingleProductPage.java
+++ app/src/main/java/com/example/myapplication/SingleProductPage.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,14 +14,18 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class
 
@@ -52,7 +57,7 @@ SingleProductPage extends AppCompatActivity {
             productprice = "$" + extras.getString("mPrice");
             productdescription = extras.getString("mDesc");
             productid = extras.getString("mId");
-            storeid =  extras.getString("StoreName");
+            storeid = extras.getString("StoreName");
         }
 
         TextView tv = (TextView) findViewById(R.id.ProductNameText);
@@ -102,45 +107,109 @@ SingleProductPage extends AppCompatActivity {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 String uid = user.getUid();
                 DatabaseReference ref = FirebaseDatabase.getInstance("https://b07project-39fda-default-rtdb.firebaseio.com/").getReference();
-                 if (ref.child("Products").child(finalproductid).child(String.valueOf("store")) != ref.child("Store").child(finalstoreid).child(String.valueOf("storeID"))) {
+                //DatabaseReference data = ref.child("Products").child(finalproductid );
 
-                LayoutInflater inflater = (LayoutInflater)
-                        getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupView = inflater.inflate(R.layout.addtocart_popupbox, null);
-                int width = LinearLayout.LayoutParams.WRAP_CONTENT;
-                int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-                boolean focusable = true;
-                PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
-                popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot){
+            String obtained;
+            String obtained2;
+           // try{
+                obtained = snapshot.child("Products").child(finalproductid).child("store").getValue().toString();
+                obtained2 = snapshot.child("Store").child(finalstoreid).child("storeID").getValue().toString();
+//                ((obtained == null) || (obtained2 == null) || (obtained.compareTo("-1") == 0)) || (obtained2.compareTo("-1") == 0)
+                if(!((obtained == null) || (obtained2 == null)) || (((obtained.compareTo("-1") == 0)) || (obtained2.compareTo("-1") == 0))){
+                    if(obtained != obtained2){
+                        LayoutInflater inflater = (LayoutInflater)
+                            getSystemService(LAYOUT_INFLATER_SERVICE);
+                    View popupView = inflater.inflate(R.layout.addtocart_popupbox, null);
+                    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    boolean focusable = true;
+                    PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+                    popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
                     yes = (Button) popupView.findViewById(R.id.YesButton);
                     yes.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
                             ref.child("Users").child(uid).child("cart").setValue(null);
                             ref.child("Users").child(uid).child("cart").child(String.valueOf(finalproductid)).setValue(count);
-                           popupWindow.dismiss();
+                            popupWindow.dismiss();
                         }
                     });
-                no = (Button) popupView.findViewById(R.id.NoButton);
-                no.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        popupWindow.dismiss();
+                    no = (Button) popupView.findViewById(R.id.NoButton);
+                    no.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            popupWindow.dismiss();
+                        }
+                    });
                     }
-                });
- } else {
-                if (count != 0) {
-                    ref.child("Users").child(uid).child("cart").child(String.valueOf(finalproductid)).setValue(count);
-                    Intent intent = new Intent(SingleProductPage.this, PInfo.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(SingleProductPage.this, "Please add an item into your cart", Toast.LENGTH_SHORT).show();
+                    else{
+                        if (count != 0) {
+                        ref.child("Users").child(uid).child("cart").child(String.valueOf(finalproductid)).setValue(count);
+                        Intent intent = new Intent(SingleProductPage.this, PInfo.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(SingleProductPage.this, "Please add an item into your cart", Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                    // -> WRITE THE CODE HERE
+                    // router(true, obtained);
                 }
 
+           // }catch(Exception e){
+                //FirebaseAuth.getInstance().signOut();
+
+           // }
+        }
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            Log.d("Error", "Cannot connect to server and set type.");
+        }
+    });
+
+
+//                if (ref.child("Products").child(finalproductid).child(String.valueOf("store")) != ref.child("Store").child(finalstoreid).child(String.valueOf("storeID"))) {
+//
+//                    LayoutInflater inflater = (LayoutInflater)
+//                            getSystemService(LAYOUT_INFLATER_SERVICE);
+//                    View popupView = inflater.inflate(R.layout.addtocart_popupbox, null);
+//                    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+//                    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+//                    boolean focusable = true;
+//                    PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+//                    popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+//                    yes = (Button) popupView.findViewById(R.id.YesButton);
+//                    yes.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            ref.child("Users").child(uid).child("cart").setValue(null);
+//                            ref.child("Users").child(uid).child("cart").child(String.valueOf(finalproductid)).setValue(count);
+//                            popupWindow.dismiss();
+//                        }
+//                    });
+//                    no = (Button) popupView.findViewById(R.id.NoButton);
+//                    no.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            popupWindow.dismiss();
+//                        }
+//                    });
+//                } else {
+//                    if (count != 0) {
+//                        ref.child("Users").child(uid).child("cart").child(String.valueOf(finalproductid)).setValue(count);
+//                        Intent intent = new Intent(SingleProductPage.this, PInfo.class);
+//                        startActivity(intent);
+//                    } else {
+//                        Toast.makeText(SingleProductPage.this, "Please add an item into your cart", Toast.LENGTH_SHORT).show();
+//                    }
+//
+//                }
             }
-             }
         });
     }
+
 }
 
 
